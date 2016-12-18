25db0a9acfab86bd754675ee1edea3cad624fc28
ctessier <clement.tessier@ymail.com>
2016-12-18 16:09:51 +0100

    Add users and consumers seeds

--- app/ConsumerStatus.php
+++ app/ConsumerStatus.php
@@ -6,9 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class ConsumerStatus extends Model
 {
-    const TEST = 1;
-    const PRIVILEGED = 2;
+    const TEST_PROGRAM = 1;
+    const PRIVILEGED_CUSTOMER = 2;
     const STOPPED = 3;
-    const PAUSED = 4;
+    const IN_BREAK = 4;
     const OTHER = 5;
 }
---
+++ database/seeds/ConsumersTableSeeder.php
@@ -0,0 +1,121 @@
+<?php
+
+use App\ConsumerStatus;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+class ConsumersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create('fr_FR');
+
+        // Seed consumers
+        DB::table('consumers')->insert([
+            [
+                'id'          => 1,
+                'first_name'  => $faker->firstNameMale,
+                'last_name'   => $faker->lastName,
+                'birthdate'   => '1985-10-07',
+                'sex'         => 'm',
+                'email'       => $faker->email,
+                'phone'       => str_replace(' ', '', $faker->mobileNumber),
+                'address'     => $faker->streetAddress,
+                'postal_code' => str_replace(' ', '', $faker->postcode),
+                'city'        => $faker->city,
+                'user_id'     => 1,
+            ],
+            [
+                'id'          => 2,
+                'first_name'  => $faker->firstNameFemale,
+                'last_name'   => $faker->lastName,
+                'birthdate'   => '1988-06-24',
+                'sex'         => 'f',
+                'email'       => $faker->email,
+                'phone'       => str_replace(' ', '', $faker->mobileNumber),
+                'address'     => $faker->streetAddress,
+                'postal_code' => str_replace(' ', '', $faker->postcode),
+                'city'        => $faker->city,
+                'user_id'     => 1,
+            ],
+            [
+                'id'          => 3,
+                'first_name'  => $faker->firstNameMale,
+                'last_name'   => $faker->lastName,
+                'birthdate'   => '1990-07-03',
+                'sex'         => 'm',
+                'email'       => $faker->email,
+                'phone'       => str_replace(' ', '', $faker->mobileNumber),
+                'address'     => $faker->streetAddress,
+                'postal_code' => str_replace(' ', '', $faker->postcode),
+                'city'        => $faker->city,
+                'user_id'     => 1,
+            ],
+            [
+                'id'          => 4,
+                'first_name'  => $faker->firstNameFemale,
+                'last_name'   => $faker->lastName,
+                'birthdate'   => '1976-05-07',
+                'sex'         => 'f',
+                'email'       => $faker->email,
+                'phone'       => str_replace(' ', '', $faker->mobileNumber),
+                'address'     => $faker->streetAddress,
+                'postal_code' => str_replace(' ', '', $faker->postcode),
+                'city'        => $faker->city,
+                'user_id'     => 2,
+            ],
+            [
+                'id'          => 5,
+                'first_name'  => $faker->firstNameMale,
+                'last_name'   => $faker->lastName,
+                'birthdate'   => '1994-01-16',
+                'sex'         => 'm',
+                'email'       => $faker->email,
+                'phone'       => str_replace(' ', '', $faker->mobileNumber),
+                'address'     => $faker->streetAddress,
+                'postal_code' => str_replace(' ', '', $faker->postcode),
+                'city'        => $faker->city,
+                'user_id'     => 2,
+            ],
+        ]);
+
+        // Seed consumers statuses
+        DB::table('consumers_consumer_statuses')->insert([
+            [
+                'consumer_id' => 1,
+                'status_id'   => ConsumerStatus::TEST_PROGRAM,
+                'date'        => '2014-05-03',
+            ],
+            [
+                'consumer_id' => 1,
+                'status_id'   => ConsumerStatus::PRIVILEGED_CUSTOMER,
+                'date'        => '2014-06-23',
+            ],
+            [
+                'consumer_id' => 2,
+                'status_id'   => ConsumerStatus::PRIVILEGED_CUSTOMER,
+                'date'        => '2010-10-30',
+            ],
+            [
+                'consumer_id' => 3,
+                'status_id'   => ConsumerStatus::TEST_PROGRAM,
+                'date'        => '2016-12-05',
+            ],
+            [
+                'consumer_id' => 4,
+                'status_id'   => ConsumerStatus::TEST_PROGRAM,
+                'date'        => '2016-11-29',
+            ],
+            [
+                'consumer_id' => 5,
+                'status_id'   => ConsumerStatus::STOPPED,
+                'date'        => '2016-09-19',
+            ],
+        ]);
+    }
+}
