0aae1709d16093088577a1412b739959b68ee9a8
ctessier <clement.tessier@ymail.com>
2016-12-21 23:14:49 +0100

    Add form validation test for update profile

--- tests/ProfileControllerTest.php
+++ tests/ProfileControllerTest.php
@@ -3,14 +3,39 @@
 class ProfileControllerTest extends TestCase
 {
     /**
+     * Test profile update form validation.
+     *
+     * @return void
+     */
+    public function test_invalid_form()
+    {
+        $this->be(\App\User::first());
+
+        $this->visitRoute('profile.show')
+            ->type('', 'name')
+            ->type('', 'email')
+            ->press('Enregistrer')
+            ->seeRouteIs('profile.show')
+            ->see('Le champ Nom est obligatoire.')
+            ->see('Le champ Adresse e-mail est obligatoire.');
+
+        $this->visitRoute('profile.show')
+            ->type('Ok', 'name')
+            ->type('test', 'email')
+            ->press('Enregistrer')
+            ->seeRouteIs('profile.show')
+            ->see('Le champ Adresse e-mail doit être une adresse e-mail valide.');
+
+    }
+
+    /**
      * Test user profile updating.
      *
      * @return void
      */
     public function test_it_updates_user_profile()
     {
-        $user = \App\User::first();
-        $this->be($user);
+        $this->be(\App\User::first());
 
         $data = [
             'name'  => 'Clark Kent',
