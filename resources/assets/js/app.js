91517988c0c57142aa5a4c6e6a0881e325b4580e
ctessier <clement.tessier@ymail.com>
2017-01-21 22:17:24 +0100

    Add confirm message on delete

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,5 +1,18 @@
 (function($) {
     $(function() {
-        // put your code here
+        initDeleteConfirmation();
     });
 }(window.jQuery));
+
+function initDeleteConfirmation()
+{
+    $('*[data-delete]').click(function (e) {
+        var message = $(this).attr('data-delete');
+
+        if (message.length === 0) {
+            message = 'Are you sure you want to delete this item?';
+        }
+
+        return confirm(message);
+    });
+}
