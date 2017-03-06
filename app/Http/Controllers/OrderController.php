c1e2fec2c0d1258f6744db45d01feed78a33a10e
ctessier <clement.tessier@ymail.com>
2017-03-06 20:35:09 +0100

    Fix styling

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -94,7 +94,7 @@ class OrderController extends Controller
      * Update the specified resource in storage.
      *
      * @param \Illuminate\Http\Request $request
-     * @param int $id
+     * @param int                      $id
      *
      * @return \Illuminate\Http\Response
      */
