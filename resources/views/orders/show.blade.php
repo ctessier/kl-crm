b2f5465186aab18e1aaa142dedb30621f79735fd
ctessier <clement.tessier@ymail.com>
2017-03-06 20:32:04 +0100

    Update orders views

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -16,13 +16,50 @@
 @section('content')
 
     <div class="row">
-        <div class="col-md-6 col-sm-12">
+        <div class="col-md-12">
             <div class="box">
                 <div class="box-header">
                     <h3 class="box-title">{{ trans('title.general-information') }}</h3>
                 </div>
                 <div class="box-body">
                     {{ $order->reference }}
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4 col-xs-12">
+            <div class="box">
+                <div class="box-header with-border">
+                    <h3 class="box-title">{{ trans('title.consumer-orders-list') }}</h3>
+                </div>
+                <div class="box-body table-responsive no-padding">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>{{ trans('label.consumer') }}</th>
+                                <th>Stock ?</th>
+                                <th>Nombre de boîte</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($order->consumer_orders as $consumer_order)
+                                <tr>
+                                    <td>{{ $consumer_order->consumer->full_name }}</td>
+                                    <td></td>
+                                    <td>{{ $consumer_order->products()->sum('quantity') }}</td>
+                                    <td>
+                                        {{ link_to_route('consumer_orders.show', trans('actions.view'), $consumer_order->id, ['class' => 'btn btn-xs btn-default']) }}
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                <div class="box-footer">
+                    {{ link_to_route('consumer_orders.create', trans('actions.new'), [], ['class' => 'btn btn-primary pull-left']) }}
                 </div>
             </div>
         </div>
