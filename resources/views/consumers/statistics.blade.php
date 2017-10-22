f29b1ba057c84d16922245614fe8aec3cbac5252
Clément Tessier <clement.tessier@ymail.com>
2017-10-22 16:24:24 +0200

    Remove stat

--- resources/views/consumers/statistics.blade.php
+++ resources/views/consumers/statistics.blade.php
@@ -2,22 +2,11 @@
     <div class="col-lg-3 col-xs-6">
         <div class="small-box bg-aqua">
             <div class="inner">
-                <h3>4</h3>
+                <h3>2</h3>
                 <p>consommateurs actifs</p>
             </div>
             <div class="icon">
                 <i class="fa fa-check"></i>
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-3 col-xs-6">
-        <div class="small-box bg-purple">
-            <div class="inner">
-                <h3>34 ans</h3>
-                <p>de moyenne d'age</p>
-            </div>
-            <div class="icon">
-                <i class="fa fa-users"></i>
             </div>
         </div>
     </div>
